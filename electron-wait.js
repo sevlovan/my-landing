10fe197398bf74f8943d5a92ad347022641e514d
sevlovan <sevlovan@gmail.com>
2026-06-22 19:21:57 +0300

    vite

--- electron-wait.js
+++ electron-wait.js
@@ -1,22 +1,7 @@
 const { spawn } = require('child_process')
-const net = require('net')
 
-function waitForPort(port, cb) {
-  const attempt = () => {
-    const client = new net.Socket()
-    client.connect(port, '127.0.0.1', () => {
-      client.destroy()
-      cb()
-    })
-    client.on('error', () => {
-      client.destroy()
-      setTimeout(attempt, 500)
-    })
-  }
-  attempt()
-}
-
-waitForPort(5173, () => {
+// Wait for Vite to be ready, then launch Electron
+setTimeout(() => {
   const proc = spawn('electron', ['.'], { stdio: 'inherit', shell: true })
   proc.on('close', code => process.exit(code ?? 0))
-})
+}, 4000)
